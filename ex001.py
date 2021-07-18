a2cb6248470dd5cbad6866b3ab7589ca41655b34
Lucas vinciguerra <87593915+LucasMateus1@users.noreply.github.com>
2021-07-18 09:27:07 -0300

    Add files via upload

--- ex001.py
+++ ex001.py
@@ -1,17 +1,18 @@
-def media_do_aluno (name, a, b):
+#função calculadora de média individual
+def media_do_aluno (name, a, b): 
     nome = name
     notas = b
     quantidade_provas= b
     media = a/b
     print('A nota do aluno {} é {:.3f}'.format(nome, media))
-
+#Variáveis
 turma = []
 contador = 0
 alunoturma1 = float(input('Há quantos alunos em sua turma?'))
 alunoturma = alunoturma1 + 1
-
+#listagem de notas da turma
 while True:
-    contador = contador + 1
+    contador += 1
     if contador == alunoturma:
         break
     else:
@@ -20,12 +21,15 @@ while True:
         print('')
         turma.append(float(input('digite a nota arredondada do aluno: ')))
         
+#soma da Lista de notas da turma        
 media = sum(turma) / alunoturma
 
+#exibições finais
 print('')
 
 print('Notas dos alunos: {}'.format(turma))
 if media >= 6:
-    print('a média da turma é {}, parabéns!!!'.format(media))
+    print('a média da turma é {:.2f}, parabéns!!!'.format(media))
 else:
-    print('a média da turma é {}, dedique-se mais!'.format(media))
+    print('a média da turma é {:.2f}, está abaixo do esperado!'.format(media))
+#fim de código
